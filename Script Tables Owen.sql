4e7a5d3542eb56baf34770904b0f72fbffaeb16a
Owen Ramirez <118354454+OwenRamirez97@users.noreply.github.com>
2023-02-06 20:43:56 -0300

    Update Script Tables Owen.sql

--- Script Tables Owen.sql
+++ Script Tables Owen.sql
@@ -1,81 +1,137 @@
--- Schema del proyect final
+-- Schema del proyecto final
 
-		create schema Proyecto_final;
+		Create schema centro_logistico;
         
         
-					-- Tablas
+		-- Tablas
 
 
+-- Proveedores
+/*
+proceso de ingreso de la mercadería dentro del centro logistico
+*/
 
-		-- PROVEEDORES, proceso de ingreso de la mercadería dentro del centro logistico
-
-create table proveedores (
-	id						INT NOT NULL PRIMARY KEY,
-	nombre_proveedores 		VARCHAR(50),
-	descripcion_articulo 	VARCHAR(50),
-    cod_articulo			INT,
-    FOREIGN KEY (cod_articulo) REFERENCES mercaderia (id_articulo)
-);
-
-
-
-		-- MERCADERÍA, Tabla dedicada a los detalles y caracteristicas de la mercadería
-
-create table mercaderia (
-	id_articulo				INT NOT NULL PRIMARY KEY,
-	sector_mercaderia 		VARCHAR(20),
-	picking			 		VARCHAR(12),
-	descripcion_articulo	VARCHAR(50)
+Create table Proveedores (
+	Cod_proveedores			INT 			NOT NULL PRIMARY KEY,
+	Nombre_proveedores 		VARCHAR(30) 		NOT NULL,
+	Cuil_Proveedor		 	INT			NOT NULL
 );
 
 
 
 
-		-- PEDIDOS, Tabla dedicada a cada detalle de los pedidos
-
-create table pedidos (
-		id_pedidos				INT NOT NULL PRIMARY KEY,
-		num_internos	 		INT,
-		leg_preparador	 		INT,
-		cod_articulo			INT,
-        
-    FOREIGN KEY (num_internos) 		REFERENCES transporte (id_internos),
-    FOREIGN KEY (leg_preparador) 	REFERENCES preparacion (id_preparador),
-    FOREIGN KEY (cod_articulo) 		REFERENCES mercaderia (id_articulo)
-);
+	-- MERCADERÍA
+/* 
+    Tabla dedicada a los detalles y caracteristicas de la mercadería
+*/
 
 
-
-		-- PREPARACIÓN, Tabla dedicada a la preparación de los pedidos
-
-	create table preparacion (
-	id_preparador				INT NOT NULL PRIMARY KEY,
-	nombre_preparador 			VARCHAR(30),
-	apellido_preparador	 		VARCHAR(30)
-);
+Create table Mercaderia (
+	Cod_articulo			INT 			NOT NULL PRIMARY KEY,
+	descripcion				VARCHAR(60) 	NOT NULL,
+	Proveedor			 	INT 		NOT NULL,
+   	Stock					INT 			,
+	Sector					VARCHAR(20) 	NOT NULL,
     
+   		 FOREIGN KEY (Proveedor) REFERENCES Proveedores (Cod_proveedores)
+);
+
+
+
+	-- Preparador
+    /*
+    Almacenamiento de datos de los 
+    preparadores, su legajo, nombre y apellido.
+    */
+    
+create table Preparador (
+		Leg_preparador			INT 			NOT NULL PRIMARY KEY,
+		Nombre_preparador		VARCHAR(30)		NOT NULL,
+		Apellido_preparador		VARCHAR(30)		NOT NULL
+);
+	
+ 
       
-   
-		-- TIENDAS, Tabla en la que se encuentran los detalles de la tienda y los pedidos que realizan
+      
+	-- Tienda
+/*
+Almacenamiento de datos de las tiendas, su código designado y ubicación.
+*/
 
-create table tiendas (
-			cod_tienda			INT NOT NULL PRIMARY KEY,
-			num_pedidos 		INT,
-			ubicacion		 	VARCHAR(30),
-				FOREIGN KEY	(num_pedidos) REFERENCES pedidos (id_pedidos)
+create  table Tienda (
+			Cod_Tienda			INT 			NOT NULL PRIMARY KEY,
+			Ubicacion		 	VARCHAR(50)		NOT NULL
 ); 
 
 
 
 
-		-- TRANSPORTE, Tabla dedicada a los datos de los transportistas que realizarán cada viaje desde el Centro Logistico
+		-- Camiones
+/* 
+Tabla dedicada a los datos de los transportistas que realizarán 
+cada viaje desde el Centro Logistico
+*/
 
-create table transporte (
-	id_internos			INT NOT NULL PRIMARY KEY,
-	nombre_chofer 		VARCHAR(20),
-	apellido_chofer		VARCHAR(20),
-    DNI_chofer			INT,
-    PatenteCamion		VARCHAR(30)
-); 
+create table Camiones (
+	Num_Interno			INT			NOT NULL PRIMARY KEY,
+	Patente 			VARCHAR(20) 		NOT NULL,
+	Modelo				VARCHAR(60) 		NOT NULL,
+   	Año				INT 			NOT NULL
+);
     
     
+    
+
+		-- Choferes
+/* 
+
+*/
+create table Choferes (
+	
+   	 DNI_chofer		INT		NOT NULL PRIMARY KEY,
+	Nombre_chofer		VARCHAR(30) 	NOT NULL,
+	Apellido_chofer		VARCHAR(60) 	NOT NULL
+);
+    
+  
+  
+-- Pedidos
+    /*
+Tabla dedicada a mostrar los detalles de los pedidos solicitados al CD,
+el numero del mismo, la tienda la cual lo solicita, el chofer que llevará el mismo,
+ y el interno con el que el chofer mencionado hará el viaje.
+    */
+
+
+create table Pedidos (
+	Cod_Pedido					INT	NOT NULL PRIMARY KEY,
+	Cod_Tienda		 			INT 	NOT NULL,
+	DNI_Chofer				 	INT 	NOT NULL,
+    	Interno						INT	NOT NULL,
+    
+    FOREIGN KEY (Cod_Tienda) 		REFERENCES Tienda (Cod_Tienda),
+    FOREIGN KEY (DNI_Chofer) 		REFERENCES choferes (DNI_Chofer),
+    FOREIGN KEY (Interno) 			REFERENCES camiones (Num_Interno)
+);
+    
+
+    	
+	-- Pickeos
+    /*
+    La tabla mostrará a detalle los movimientos realizados en el armado de los pedidos, 
+    es decir, los artículos y la cantidad que saldrán de los mismos a cierto pedido,
+    junto con el preparador que lo arma.
+    */
+
+create table Pickeos (
+	ID_Pickeo				INT 	NOT NULL PRIMARY KEY auto_increment,
+	Articulo_pickeado 			INT 	NOT NULL,
+	Pedido				 	INT	NOT NULL,
+    Cantidad					INT		,
+    Preparador					INT	NOT NULL,
+    
+    FOREIGN KEY (Articulo_pickeado) 		REFERENCES mercaderia (Cod_articulo),
+    FOREIGN KEY (Pedido) 					REFERENCES pedidos (Cod_Pedido),
+    FOREIGN KEY (Preparador) 				REFERENCES preparador (Leg_preparador)
+);
+
